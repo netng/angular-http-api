d5cc4acfe7e77a21d2e8696854b40129fffd82f3
nan <nandangsopyan08@gmail.com>
2023-07-13 11:06:21 +0700

    http report progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,147 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
+import { User } from './interfaces/user';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-http';
+  private user: User = {
+    'id': 5,
+    'name': 'Nan Super',
+    'username': 'supernan',
+    'email': 'Sincere@april.biz',
+    'address': {
+      'street': 'Kulas Light',
+      'suite': 'Apt. 556',
+      'city': 'Gwenborough',
+      'zipcode': '92998-3874',
+      'geo': {
+        'lat': '-37.3159',
+        'lng': '81.1496'
+      }
+    },
+    'phone': '1-770-736-8031 x56442',
+    'website': 'hildegard.org',
+    'company': {
+      'name': 'Romaguera-Crona',
+      'catchPhrase': 'Multi-layered client-server neural-net',
+      'bs': 'harness real-time e-markets'
+    }
+  };
+
+  fileStatus = { status: '', percentage: 0 };
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.onGetUsers();
+    // this.onGetUser();
+    // this.onCreateUser();
+    // this.onUpdateUser();
+    // this.onDeleteUser();
+  }
+
+  onGetUsers(): void {
+    this.userService.getUsers().subscribe({
+      next: ((response: HttpEvent<User[]>) => {
+        console.log(response);
+      }),
+      error: ((error: any) => {
+        console.log(error);
+      }),
+      complete: () => {
+        console.log('Getting users done');
+      }
+    })
+  }
+
+  onGetUser(): void {
+    this.userService.getUser().subscribe({
+      next: ((response: User) => {
+        console.log(response);
+      }),
+      error: ((error: any) => {
+        console.log(error);
+      }),
+      complete: () => {
+        console.log('Getting user done');
+      }
+    })
+  }
+
+  onCreateUser(): void {
+    this.userService.createUser(this.user).subscribe({
+      next: (response: User) => {
+        console.log(response);
+      },
+      error: (error: any) => {
+        console.log(error);
+      },
+      complete: () => console.log('Done creating user')
+    })
+  }
+
+  onUpdateUser(): void {
+    this.userService.updateUser(this.user).subscribe({
+      next: (response: User) => {
+        console.log(response);
+      },
+      error: (error: any) => {
+        console.log(error);
+      },
+      complete: () => console.log('Done updating user')
+    })
+  }
+
+  onDeleteUser(): void {
+    this.userService.deleteUser(this.user).subscribe({
+      next: ((response) => {
+        console.log(response);
+      }),
+      error: ((error) => {
+        console.error('something goes wrong: ', error);
+      }),
+      complete: (() => console.log('deleting done'))
+    })
+  }
+
+  onUploadFile(files: File[]): void {
+    console.log(files);
+    const formData = new FormData();
+    for(const file of files) {
+      formData.append('file', file, file.name);
+    }
+
+    this.userService.upladoFile(formData).subscribe({
+      next: ((event: HttpEvent<string>) => {
+        switch(event.type) {
+          case HttpEventType.UploadProgress || HttpEventType.DownloadProgress:
+            console.log(event);
+            this.fileStatus.percentage = Math.round(100 * event.loaded / event.total!);
+            this.fileStatus.status = 'progress';
+            console.log(this.fileStatus);
+            break;
+          case HttpEventType.Response:
+            if(event.status === 201) {
+              console.log(event.status);
+              this.fileStatus.percentage = 0;
+              this.fileStatus.status = 'done';
+              console.log(this.fileStatus);
+              break;
+            }
+        }
+      }),
+      error: ((error) => {
+        console.log('something goes wrong ', error);
+      }),
+      complete: () => console.log('uploading done')
+
+    })
+
+  }
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpEvent, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
+import { environment } from 'src/environments/environment.development';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = environment.apiUrl;
+  readonly arrayParams: (string | number)[] = ['list1', 'list2', 2];
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<HttpEvent<User[]>> {
+    let myParams = new HttpParams({fromObject: {['list']: ['list1', 'list2']} });
+    myParams = myParams.append('name', 'fus')
+    return this.http.get<User[]>(`${this.apiUrl}/users`,
+    { observe: 'events', reportProgress: true });
+  }
+
+  getUser(): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/users/1`);
+  }
+
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/users`, user);
+  }
+
+  updateUser(user: User): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/users/${user.id}`, user)
+  }
+
+  deleteUser(user: User): Observable<void> {
+    let myHeader = new HttpHeaders({'myheader': ['headervalue', 'headervalue2']});
+    myHeader = myHeader.set('X-POWERED-BY', 'FUSDEV');
+    return this.http.delete<void>(`${this.apiUrl}/users/${user.id}`, { headers: myHeader })
+  }
+
+  upladoFile(file: FormData): Observable<HttpEvent<string>> {
+    return this.http.post<any>(`http://localhost:3000/report/income/local`, file, { observe: 'events', reportProgress: true })
+  }
+}
